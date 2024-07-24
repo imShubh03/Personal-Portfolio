a77e41fc6fe16276e9944fef8aee7af533d9ae26
Shubham7476 <shubhamsonake307@gmail.com>
2024-07-24 17:06:08 +0530

    frontend

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GiCalendar, GiClawSlashes, GiMailbox, GiMayanPyramid, GiPhone } from "react-icons/gi";
 import { MdOutlineContactPage } from "react-icons/md";
+import Socials from "./Socials";
 
 const Aside = () => {
     return (
@@ -16,7 +17,7 @@ const Aside = () => {
                 </figure>
 
                 <div className="info-content">
-                    <h1 className="name" title="Richard Hanrick">
+                    <h1 className="name" title="Shubham Sonake">
                         Shubham Sonake
                     </h1>
 
@@ -26,45 +27,27 @@ const Aside = () => {
                 <button className="info_more-btn" data-sidebar-btn>
                     <span>Show Contacts</span>
 
+                    <div className="hidden lg:block"> 
                     <GiClawSlashes />
+                    </div>
+
+                    <div className="block lg:hidden"> {/* This ensures Socials is only visible on screens smaller than md */}
+                        <Socials />
+                    </div>
                 </button>
             </div>
 
             <hr />
 
-
-
             <div className=" my-4">
-                <h3 className="text-white ml-7 mb-3 font-thin text-xs mb-2">Connect withme over👇 </h3>
-                <ul className="social-list">
-                    <li className="social-item">
-                        <a href="https://www.linkedin.com/in/shubham-sonake-410241231" className="social-link">
-                            <ion-icon name="logo-linkedin"></ion-icon>
-                        </a>
-                    </li>
-                    <li className="social-item">
-                        <a href="https://github.com/imShubh03" className="social-link">
-                            <ion-icon name="logo-github"></ion-icon>
-                        </a>
-                    </li>
-                    <li className="social-item">
-                        <a href="https://www.instagram.com/shubham17?igsh=MTdlaGpjeTR6NzQ1aw==" className="social-link">
-                            <ion-icon name="logo-instagram"></ion-icon>
-                        </a>
-                    </li>
-                    <li className="social-item">
-                        <a href="https://x.com/Shubham3102003?s=08" className="social-link">
-                            <ion-icon name="logo-twitter"></ion-icon>
-                        </a>
-                    </li>
-                </ul>
+                <h3 className="text-white ml-7 font-thin text-xs mb-2">Connect withme over👇 </h3>
+                <Socials />
             </div>
 
             <hr />
 
             <div className="sidebar-info_more">
                 <div className="separator"></div>
-
                 <ul className="contacts-list">
                     <li className="contact-item">
                         <div className="icon-box">
@@ -96,7 +79,7 @@ const Aside = () => {
                             </div>
                             <div className="contact-info">
                                 <p className="contact-title">Resume</p>
-                                <time dateTime="2003-10-03"> Click to See My CV</time>
+                                <p className=" text-xs text-white font-thin"> Click to See My CV</p>
                             </div>
                         </li>
                     </a>
---
+++ src/components/Socials.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Socials() {
+    return (
+        <>
+            <ul className="social-list">
+                <li className="social-item">
+                    <a href="https://www.linkedin.com/in/shubham-sonake-410241231" className="social-link">
+                        <ion-icon name="logo-linkedin"></ion-icon>
+                    </a>
+                </li>
+                <li className="social-item">
+                    <a href="https://github.com/imShubh03" className="social-link">
+                        <ion-icon name="logo-github"></ion-icon>
+                    </a>
+                </li>
+                <li className="social-item">
+                    <a href="https://www.instagram.com/shubham17?igsh=MTdlaGpjeTR6NzQ1aw==" className="social-link">
+                        <ion-icon name="logo-instagram"></ion-icon>
+                    </a>
+                </li>
+                <li className="social-item">
+                    <a href="https://x.com/Shubham3102003?s=08" className="social-link">
+                        <ion-icon name="logo-twitter"></ion-icon>
+                    </a>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default Socials
